fb1a438ed5c1ec39b0910fa693c12aa5284cac40
abuanwar072 <abuanwar072@gmail.com>
2020-02-22 17:34:59 +0600

    user 80% done

--- colbd/settings.py
+++ colbd/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
     # My apps
     'info.apps.InfoConfig',
     'migration.apps.MigrationConfig',
--- info/models.py
+++ info/models.py
@@ -15,10 +15,13 @@ STATUS_CHOICES = (
 
 
 class Info(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, blank=True, null=True)
+    # col_id = models.CharField(max_length=250, blank=True, unique=True)
     first_name = models.CharField(max_length=200)
     last_name = models.CharField(max_length=200, blank=False)
     pn_number = models.IntegerField()
+    email = models.EmailField(blank=True)
     birth_date = models.DateField(blank=True)
     status = models.CharField(
         max_length=20, choices=STATUS_CHOICES, default='1')
@@ -28,6 +31,8 @@ class Info(models.Model):
     address = models.CharField(max_length=800)
     package = models.ForeignKey(Package, on_delete=models.SET_NULL, null=True)
     ref_by = models.CharField(max_length=50, blank=True)
+    latitude = models.CharField(max_length=50, blank=True)
+    longitude = models.CharField(max_length=50, blank=True)
     active_on = models.DateField(blank=True, null=True)
     expired_at = models.DateField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now=True)

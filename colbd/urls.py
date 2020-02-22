405c1eb36b01c551a0d2d77a63d44da9e5e471c7
abuanwar072 <abuanwar072@gmail.com>
2020-02-22 21:10:30 +0600

    package done

--- colbd/urls.py
+++ colbd/urls.py
@@ -19,4 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('info.urls')),
+    path('api/package/', include('package.urls')),
 ]
